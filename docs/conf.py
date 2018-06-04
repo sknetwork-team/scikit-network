668ad938ec98c2ddad0e3a61fafc909cfff5bd0b
Bertrand Charpentier <bertrand.charpentier@live.fr>
2018-06-04 11:37:27 +0200

    Other tests to have the doc of linkage clustering
    -remove two other unknow sphynx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.napoleon',
               'sphinx.ext.todo',
-              'sphinx.ext.viewcode',
-              'nb2plots',
-              'texext',]
+              'sphinx.ext.viewcode',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
