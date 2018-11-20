5f990d9dfdd7411a64012492c6740c8a861026ad
Marc <marc.lelarge@ens.fr>
2018-11-20 19:15:02 +0100

    updated dataloader for ca-CondMat

--- sknetwork/loader/loaders.py
+++ sknetwork/loader/loaders.py
@@ -3,10 +3,12 @@ from sknetwork.model.graph import Graph
 import os
 import os.path
 import zipfile
+import tarfile
 
 from six.moves import urllib
 
-urls = {'polblogs.mtx': 'http://nrvis.com/download/data/dimacs10/polblogs.zip'}
+urls = {'polblogs.mtx': 'http://nrvis.com/download/data/dimacs10/polblogs.zip',
+        'ca-CondMat.mtx': 'https://sparse.tamu.edu/MM/SNAP/ca-CondMat.tar.gz'}
 
 class Dataset():
     def __init__(self, file_name, directed = True, download = True, root='./data/'):
@@ -20,31 +22,42 @@ class Dataset():
 
         self.make_dir()
 
-        file_path = os.path.join(root, file_name)
+        self.file_path = os.path.join(root, file_name)
 
         if download:
-            self.download(file_name, file_path)
+            self.download(file_name)
 
         if file_name[-3:] == 'mtx':
             #print(file_path)
-            self.load_from_mtx(file_path)
+            self.load_from_mtx(self.file_path)
 
     def make_dir(self):
         #root = os.path.expanduser(root)
         if not os.path.exists(self.root):
             os.makedirs(self.root)
 
-    def download(self, file_name, file_path):
-        if not os.path.exists(file_path):
+    def download(self, file_name):
+        if not os.path.exists(self.file_path):
             if urls[file_name][-4:] == '.zip':
-                urllib.request.urlretrieve(urls[file_name], file_path[:-3]+'zip')
+                urllib.request.urlretrieve(urls[file_name], self.file_path[:-3]+'zip')
                 try:
-                    zip_ref = zipfile.ZipFile(file_path[:-3]+'zip', 'r')
+                    zip_ref = zipfile.ZipFile(self.file_path[:-3]+'zip', 'r')
                     zip_ref.extract(file_name, self.root)
                     zip_ref.close()
                 except ValueError:
                     print('Pb with the zip file')
-                return file_path
+            elif urls[file_name][-6:] == 'tar.gz':
+                urllib.request.urlretrieve(urls[file_name], self.file_path[:-3] + 'tar.gz')
+                try:
+                    tar = tarfile.open(self.file_path[:-3] + 'tar.gz', "r:gz")
+                    for member in tar.getmembers():
+                        #print(member.name)
+                        l = len(file_name)
+                        if member.name[-l:] == file_name:
+                            tar.extract(member, path=self.root)
+                            self.file_path = os.path.join(self.root,member.name)
+                except ValueError:
+                    print('Pb with the tar.gz file')
             else:
                 print('Not implemented')
 
@@ -66,6 +79,10 @@ class Dataset():
         >>> polblogs = Dataset('polblogs.mtx')
         loading graph with 1490 nodes and 19025 edges.
         number of nodes: 1224 (expected 1490); number of edges: 19025 (expected 19025)
+
+        >>> ca = Dataset('ca-CondMat.mtx')
+        loading graph with 23133 nodes and 93497 edges.
+        number of nodes: 23133 (expected 23133); number of edges: 93497 (expected 93497)
 
         Note
         ----
