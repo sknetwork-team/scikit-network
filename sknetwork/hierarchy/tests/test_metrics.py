5ad4ef18dbbd750b6b7446fd7cfb04645e8ea6ed
Thomas Bonald <thomas.bonald@telecom-paristech.fr>
2019-01-16 11:46:16 +0100

    Unit test metrics

--- sknetwork/hierarchy/tests/test_metrics.py
+++ sknetwork/hierarchy/tests/test_metrics.py
@@ -9,8 +9,16 @@
 
 import unittest
 from sknetwork.hierarchy.paris import Paris
+from sknetwork.toy_graphs.graph_data import GraphConstants
+from sknetwork.hierarchy.metrics import relative_entropy
 
 class TestMetrics(unittest.TestCase):
 
     def setUp(self):
         self.paris = Paris()
+        self.karate_club = GraphConstants.karate_club_graph()
+
+    def test_karate_club_graph(self):
+        graph = self.karate_club
+        dendrogram = self.paris.fit(graph).dendrogram_
+        relative_entropy(graph, dendrogram)
