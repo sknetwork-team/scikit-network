239876e854c80a9d68d08e28f80609e760e18274
Nathan DE LARA <ndelara@enst.fr>
2019-03-21 15:16:46 +0100

    release number

--- sknetwork/__init__.py
+++ sknetwork/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """scikit-network team"""
 __email__ = "bonald@enst.fr"
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from scipy.sparse.csgraph import *
 from sknetwork.toy_graphs import *
