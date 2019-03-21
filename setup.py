3232a3bfb5d71b5a5ed152adf038a7d766b62ccc
Nathan DE LARA <ndelara@enst.fr>
2019-03-21 15:44:16 +0100

    update version number

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sknetwork-team/sknetwork',
-    version='0.1.1',
+    version='0.2.0',
     zip_safe=False,
 )
