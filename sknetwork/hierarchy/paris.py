37379cf552caa31722a6b292e0f1170a8be70448
Thomas Bonald <thomas.bonald@telecom-paristech.fr>
2019-06-19 15:23:02 +0200

    Dendrogram cut

--- sknetwork/hierarchy/paris.py
+++ sknetwork/hierarchy/paris.py
@@ -293,17 +293,17 @@ class Paris:
     >>> adjacency = adjacency + adjacency.T
 
     >>> paris = Paris()
-    >>> paris.fit(adjacency).predict()
-    array([1, 1, 0, 0, 1])
+    >>> paris.dendrogram_.shape
+    (4,4)
 
     Notes
     -----
     Each row of the dendrogram = i, j, height, size of cluster i + j.
 
-    The similarity between clusters i,j is :math:`\\dfrac{w_{ij}}{w_i w_j}`
+    The similarity between clusters i,j is :math:`\\dfrac{A_{ij}}{w_i w_j}`
     where
 
-    * :math:`w_{ij}` is the weight of edge i,j in the aggregate graph
+    * :math:`A_{ij}` is the weight of edge i,j in the aggregate graph
 
     * :math:`w_{i}` is the weight of cluster i
 
@@ -432,36 +432,3 @@ class Paris:
         else:
             raise ValueError('Unknown engine.')
 
-    def predict(self, n_clusters: int = 2, sorted_clusters: bool = False) -> np.ndarray:
-        """
-        Extract the clustering with given number of clusters from the dendrogram.
-
-        Parameters
-        ----------
-        n_clusters :
-            Number of clusters.
-        sorted_clusters :
-            If True, sort labels in decreasing order of cluster size.
-
-        Returns
-        -------
-        labels : np.ndarray
-            Cluster index of each node.
-        """
-        if self.dendrogram_ is None:
-            raise ValueError("First fit data using the fit method.")
-        n_nodes = np.shape(self.dendrogram_)[0] + 1
-        if n_clusters < 1 or n_clusters > n_nodes:
-            raise ValueError("The number of clusters must be between 1 and the number of nodes.")
-
-        labels = np.zeros(n_nodes, dtype=int)
-        clusters = {node: [node] for node in range(n_nodes)}
-        for t in range(n_nodes - n_clusters):
-            clusters[n_nodes + t] = clusters.pop(int(self.dendrogram_[t][0])) + \
-                                    clusters.pop(int(self.dendrogram_[t][1]))
-        clusters = list(clusters.values())
-        if sorted_clusters:
-            clusters = sorted(clusters, key=len, reverse=True)
-        for label, cluster in enumerate(clusters):
-            labels[cluster] = label
-        return labels
