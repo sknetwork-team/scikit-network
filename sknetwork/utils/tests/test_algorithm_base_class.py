c8838d1bbc02a856d6c2bcfe092bc650c45be3aa
Quentin L <qlutz@enst.fr>
2019-06-28 13:36:06 +0200

    Corrected tests to tackle Numba ambiguity

--- sknetwork/utils/tests/test_algorithm_base_class.py
+++ sknetwork/utils/tests/test_algorithm_base_class.py
@@ -6,6 +6,7 @@ import unittest
 from sknetwork.ranking import PageRank
 from sknetwork.hierarchy import Paris
 from sknetwork.clustering import Louvain
+from sknetwork import is_numba_available
 
 
 class TestAlgo(unittest.TestCase):
@@ -14,10 +15,13 @@ class TestAlgo(unittest.TestCase):
         self.pagerank = PageRank()
         self.paris = Paris()
         self.louvain = Louvain(tol=0)
+        self.engine = is_numba_available * "numba" + (not is_numba_available) * "python"
 
     def test_reprs(self):
         self.assertEqual(str(self.pagerank), "PageRank(damping_factor=0.85, method='diter', n_iter=25)")
-        self.assertEqual(str(self.paris), "Paris(engine='python')")
-        self.assertEqual(str(self.louvain), "Louvain(algorithm=GreedyModularity(resolution=1, tol=0, engine='python'), "
+        self.assertEqual(str(self.paris), "Paris(engine='" + self.engine + "')")
+        self.assertEqual(str(self.louvain), "Louvain(algorithm=GreedyModularity(resolution=1, "
+                                            "tol=0, engine='" + self.engine + "'), "
                                             "agg_tol=0.001, max_agg_iter=-1, shuffle_nodes=False, verbose=False)")
-        self.assertEqual(str(self.louvain.algorithm), "GreedyModularity(resolution=1, tol=0, engine='python')")
+        self.assertEqual(str(self.louvain.algorithm), "GreedyModularity(resolution=1, "
+                                                      "tol=0, engine='" + self.engine + "')")
