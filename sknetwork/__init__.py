9a02c16be5074a6157a6f1905acc31248f7f0798
Quentin L <qlutz@enst.fr>
2019-07-08 09:10:17 +0200

    Idem

--- sknetwork/__init__.py
+++ sknetwork/__init__.py
@@ -9,7 +9,8 @@ __version__ = '0.7.1'
 import numpy as np
 try:
     from numba import __version__ as numba_version
-    if int(numba_version.split('.')[1]) < 44 and int(numba_version.split('.')[0]) == 0:
+    if (int(numba_version.split('.')[1]) == 44 and int(numba_version.split('.')[0]) == 0)\
+            or int(numba_version.split('.')[1]) <= 44:
         raise ImportWarning('To enable all features using Numba, please update Numba.')
     from numba import njit, prange, types
     from numba.typed import Dict as TypedDict
