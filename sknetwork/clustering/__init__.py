008cd1e899b8e496f0b56a684ddf9de8ffc241f4
Quentin L <qlutz@enst.fr>
2019-07-19 11:44:10 +0200

    Doc correction for PyPI

--- sknetwork/clustering/__init__.py
+++ sknetwork/clustering/__init__.py
@@ -1,4 +1,4 @@
-from sknetwork.clustering.louvain import Louvain, GreedyModularity
+from sknetwork.clustering.louvain import Louvain, GreedyModularity, Optimizer
 from sknetwork.clustering.bilouvain import BiLouvain
 from sknetwork.clustering.metrics import modularity, bimodularity, cocitation_modularity
 
