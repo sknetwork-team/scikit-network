dee71c6372eb330f20288447d05bf1651e4ff78d
Quentin L <qlutz@enst.fr>
2019-11-12 15:24:14 +0100

    Tests

--- sknetwork/basics/tests/test_rand_walk.py
+++ sknetwork/basics/tests/test_rand_walk.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# tests for metrics.py
 """"tests for rand_walk.py"""
 
 import unittest
--- sknetwork/basics/tests/test_structure.py
+++ sknetwork/basics/tests/test_structure.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# tests for metrics.py
 """"tests for structure.py"""
 
 import unittest
