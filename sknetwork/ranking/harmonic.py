efe53078d923626fa76e15db3ff30ea7797af2d4
Quentin L <qlutz@enst.fr>
2019-11-19 09:41:31 +0100

    Idem

--- sknetwork/ranking/harmonic.py
+++ sknetwork/ranking/harmonic.py
@@ -10,7 +10,7 @@ import numpy as np
 
 from sknetwork.utils.algorithm_base_class import Algorithm
 from sknetwork.utils.checks import check_format, is_square
-from sknetwork.basics import shortest_path, is_connected
+from sknetwork.basics import shortest_path
 
 
 class Harmonic(Algorithm):
