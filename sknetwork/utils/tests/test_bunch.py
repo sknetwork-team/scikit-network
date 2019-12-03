c9d082e50282d3455516a79c66a1f59a490be38c
Quentin L <qlutz@enst.fr>
2019-12-03 13:21:06 +0100

    Test  for Bunch

---
+++ sknetwork/utils/tests/test_bunch.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from sknetwork.utils import Bunch
+
+
+class TestBunch(unittest.TestCase):
+
+    def test_key_error(self):
+        bunch = Bunch(a=1, b=2)
+        with self.assertRaises(AttributeError):
+            # noinspection PyStatementEffect
+            bunch.c
