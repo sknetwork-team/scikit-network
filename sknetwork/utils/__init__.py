2d447b15e81113324a24905e1f74c33d1c45d89f
Quentin L <qlutz@enst.fr>
2019-12-03 13:13:15 +0100

    Simplified Bunch

--- sknetwork/utils/__init__.py
+++ sknetwork/utils/__init__.py
@@ -29,15 +29,8 @@ class Bunch(dict):
     def __setattr__(self, key, value):
         self[key] = value
 
-    def __dir__(self):
-        return self.keys()
-
     def __getattr__(self, key):
         try:
             return self[key]
         except KeyError:
             raise AttributeError(key)
-
-    def __setstate__(self, state):
-        pass
-
