82e5fbb0f98884aa77ff16eb699556def24920f3
Quentin L <qlutz@enst.fr>
2019-12-03 11:51:24 +0100

    Verbosity Mixin

---
+++ sknetwork/utils/verbose.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Dec 3, 2019
+@author: Quentin Lutz <qlutz@enst.fr>
+"""
+
+
+class Log:
+    """
+    Log class for easier verbosity features
+    """
+    def __init__(self, verbose: bool = False):
+        self.verbose = verbose
+        self.log = ''
+
+    def print(self, *args):
+        if self.verbose:
+            print(*args)
+        self.log += ' '.join(map(str, args)) + '\n'
+
+
+class VerboseMixin:
+    """
+    Mixin class for verbosity
+    """
+    def __init__(self, verbose: bool = False):
+        self.log = Log(verbose)
