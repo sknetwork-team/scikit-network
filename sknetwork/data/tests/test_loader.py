b9a6bc91fb85acfba58644b3637270d6d24f8e69
Quentin L <qlutz@enst.fr>
2020-01-07 09:51:27 +0100

    Patch for network availability

--- sknetwork/data/tests/test_loader.py
+++ sknetwork/data/tests/test_loader.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 """tests for loading.py"""
 
+from urllib.error import URLError
 import unittest
 import tempfile
+import warnings
 
 from sknetwork.data import load_wikilinks_dataset, load_konect_dataset, clear_data_home
 
@@ -13,7 +15,11 @@ class TestLoader(unittest.TestCase):
     def test_wikilinks(self):
         tmp_data_dir = tempfile.gettempdir() + '/stub'
         clear_data_home(tmp_data_dir)
-        data = load_wikilinks_dataset('stub', tmp_data_dir)
+        try:
+            data = load_wikilinks_dataset('stub', tmp_data_dir)
+        except URLError:
+            warnings.warn('Could not reach Telecom Graphs. Corresponding test has not been performed.', RuntimeWarning)
+            return
         self.assertEqual(data.biadjacency.shape[0], 1)
         self.assertEqual(data.names.shape[0], 2)
         clear_data_home(tmp_data_dir)
@@ -21,7 +27,11 @@ class TestLoader(unittest.TestCase):
     def test_konect(self):
         tmp_data_dir = tempfile.gettempdir() + '/moreno_crime'
         clear_data_home(tmp_data_dir)
-        data = load_konect_dataset('moreno_crime', tmp_data_dir)
+        try:
+            data = load_konect_dataset('moreno_crime', tmp_data_dir)
+        except URLError:
+            warnings.warn('Could not reach Konect. Corresponding test has not been performed.', RuntimeWarning)
+            return
         self.assertEqual(data.biadjacency.shape[0], 829)
         self.assertEqual(data.name.shape[0], 829)
         data = load_konect_dataset('moreno_crime', tmp_data_dir)
