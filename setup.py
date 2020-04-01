4073047acbf8d6f1b0147d42d01daa431533f64b
Unknown <nathan.de-lara@polytechnique.edu>
2020-04-01 15:21:54 +0200

    change path

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = ['pytest', 'nose', 'pluggy>=0.7.1']
 
 pyx_paths = ["sknetwork/utils/", "sknetwork/clustering/"]
 c_paths = ["sknetwork/utils/toto.cpp", "sknetwork/clustering/louvain_core.cpp"]
-modules = ['sknetwork.utils.toto', 'sknetwork.clustering.louvain_core']
+modules = ['toto', 'louvain_core']
 
 """
 try:
