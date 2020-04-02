128c3e8adbff10d5cd34a88ed85c9fea0fb4efc9
Quentin L <qlutz@enst.fr>
2020-04-02 10:35:37 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -23,14 +23,18 @@ setup_requirements = ['pytest-runner']
 test_requirements = ['pytest', 'nose', 'pluggy>=0.7.1']
 
 
-pyx_paths = ["sknetwork/utils/toto.pyx", "sknetwork/clustering/louvain_core.pyx"]
-c_paths = ["sknetwork/utils/toto.cpp", "sknetwork/clustering/louvain_core.cpp"]
-modules = ['sknetwork.utils.toto', 'sknetwork.clustering.louvain_core']
+pyx_paths = ["sknetwork/utils/knn1d.pyx", "sknetwork/clustering/louvain_core.pyx"]
+c_paths = ["sknetwork/utils/knn1d.cpp", "sknetwork/clustering/louvain_core.cpp"]
+modules = ['sknetwork.utils.knn1d', 'sknetwork.clustering.louvain_core']
 
-try:
-    import Cython
-    HAVE_CYTHON = True
-except ImportError:
+
+if os.environ.get('SKNETWORK_DISABLE_CYTHONIZE') is None:
+    try:
+        import Cython
+        HAVE_CYTHON = True
+    except ImportError:
+        HAVE_CYTHON = False
+else:
     HAVE_CYTHON = False
 
 
