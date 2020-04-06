b19ff2266d58ea1b0bf4cde9e865320f3ba362dd
Unknown <nathan.de-lara@polytechnique.edu>
2020-04-06 15:36:33 +0200

    Embedding
    * tests for metrics

--- sknetwork/embedding/tests/test_metrics.py
+++ sknetwork/embedding/tests/test_metrics.py
@@ -6,17 +6,16 @@ import unittest
 
 from sknetwork.embedding import GSVD
 from sknetwork.embedding.metrics import cosine_modularity
-from sknetwork.data.test_graphs import *
+from sknetwork.data.test_graphs import test_bigraph
 
 
 class TestClusteringMetrics(unittest.TestCase):
 
-    def setUp(self):
-        self.method = GSVD(3, solver='lanczos')
-
     def test_bipartite(self):
         biadjacency = test_bigraph()
-        embedding = self.method.fit_transform(biadjacency)
-        embedding_col = self.method.embedding_col_
+        method = GSVD(3, solver='lanczos')
+
+        embedding = method.fit_transform(biadjacency)
+        embedding_col = method.embedding_col_
         fit, div, modularity = cosine_modularity(biadjacency, embedding, embedding_col, return_all=True)
         self.assertAlmostEqual(modularity, fit - div)
