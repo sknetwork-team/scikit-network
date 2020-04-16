17fcf87d74cef7f0e91f42914bf5c9b533287a25
Unknown <nathan.de-lara@polytechnique.edu>
2020-04-16 13:17:32 +0200

    Tests for spring layout

--- sknetwork/embedding/spring.py
+++ sknetwork/embedding/spring.py
@@ -10,7 +10,7 @@ from typing import Union
 import numpy as np
 from scipy import sparse
 
-from sknetwork.embedding import GSVD
+from sknetwork.embedding.svd import GSVD
 from sknetwork.embedding.base import BaseEmbedding
 from sknetwork.utils.check import check_format, check_square, is_symmetric
 from sknetwork.utils.format import directed2undirected
@@ -19,13 +19,14 @@ from sknetwork.utils.format import directed2undirected
 class FruchtermanReingold(BaseEmbedding):
     """Basic spring layout for displaying small graphs."""
 
-    def __init__(self, strength: float = None, n_iter: int = 50, tol: float = 1e-4):
+    def __init__(self, strength: float = None, n_iter: int = 50, tol: float = 1e-4, pos_init: str = 'spectral'):
         super(FruchtermanReingold, self).__init__()
         self.strength = strength
         self.n_iter = n_iter
         self.tol = tol
+        self.pos_init = pos_init
 
-    def fit(self, adjacency: Union[sparse.csr_matrix, np.ndarray], pos_init: Union[np.ndarray, str] = 'spectral',
+    def fit(self, adjacency: Union[sparse.csr_matrix, np.ndarray], pos_init: np.ndarray = None,
             n_iter: int = None) -> 'FruchtermanReingold':
         """Compute layout"""
         adjacency = check_format(adjacency)
@@ -34,10 +35,11 @@ class FruchtermanReingold(BaseEmbedding):
             adjacency = directed2undirected(adjacency)
         n = adjacency.shape[0]
 
-        if pos_init == 'spectral':
-            pos = GSVD(n_components=2).fit_transform(adjacency)
-        elif pos_init == 'random':
-            pos = np.random.randn(n, 2)
+        if pos_init is None:
+            if self.pos_init == 'spectral':
+                pos = GSVD(n_components=2).fit_transform(adjacency)
+            else:
+                pos = np.random.randn(n, 2)
         elif isinstance(pos_init, np.ndarray):
             if pos_init.shape == (n, 2):
                 pos = pos_init.copy()
@@ -54,7 +56,9 @@ class FruchtermanReingold(BaseEmbedding):
         else:
             strength = self.strength
 
-        step_max: float = 0.1 * max(max(pos[:, 0] - min(pos[:, 0]), max(pos[:, 1] - min(pos[:, 1]))))
+        delta_x: float = pos[:, 0].max() - pos[:, 0].min()
+        delta_y: float = pos[:, 1].max() - pos[:, 1].min()
+        step_max: float = 0.1 * max(delta_x, delta_y)
         step: float = step_max / (n_iter + 1)
         delta = np.zeros((n, 2))
         for iteration in range(n_iter):
@@ -63,8 +67,8 @@ class FruchtermanReingold(BaseEmbedding):
                 indices = adjacency.indices[adjacency.indptr[i]:adjacency.indptr[i+1]]
                 data = adjacency.data[adjacency.indptr[i]:adjacency.indptr[i+1]]
 
-                grad: np.ndarray = (pos[i] - pos).ravel()
-                distance: np.ndarray = np.linalg.norm(grad, axis=1)
+                grad: np.ndarray = (pos[i] - pos)  # shape (n, 2)
+                distance: np.ndarray = np.linalg.norm(grad, axis=1)  # shape (n,)
                 distance = np.where(distance < 0.01, 0.01, distance)
 
                 attraction = np.zeros(n)
@@ -72,7 +76,7 @@ class FruchtermanReingold(BaseEmbedding):
 
                 repulsion = (strength * distance)**2
 
-                delta[i]: np.ndarray = (grad * (repulsion - attraction)).sum(axis=1)
+                delta[i]: np.ndarray = (grad * (repulsion - attraction)[:, np.newaxis]).sum(axis=0)  # shape (2,)
             length = np.linalg.norm(delta, axis=0)
             length = np.where(length < 0.01, 0.1, length)
             delta = delta * step_max / length
@@ -81,6 +85,6 @@ class FruchtermanReingold(BaseEmbedding):
             err: float = np.linalg.norm(delta) / n
             if err < self.tol:
                 break
-                
+
         self.embedding_ = pos
         return self
