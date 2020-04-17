646646908c6772c1698ef2d00f8a20daaa02da72
Unknown <nathan.de-lara@polytechnique.edu>
2020-04-17 10:33:02 +0200

    Minor

--- sknetwork/visualization/tests/test_dendrograms.py
+++ sknetwork/visualization/tests/test_dendrograms.py
@@ -39,4 +39,3 @@ class TestVisualization(unittest.TestCase):
         image = svg_dendrogram(dendrogram, names=names, width=200, height=200, margin=10, margin_text=5, scale=3,
                                n_clusters=3, color='green', font_size=14, reorder=True, rotate=True)
         self.assertEqual(image[1:4], 'svg')
-
