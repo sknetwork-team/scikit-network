4b53e7c2750f17308e8b674b70a94077b5598a5c
Thomas Bonald <thomas.bonald@telecom-paristech.fr>
2020-04-17 10:20:57 +0200

    Test dendrogram

--- sknetwork/visualization/tests/test_dendrograms.py
+++ sknetwork/visualization/tests/test_dendrograms.py
@@ -24,6 +24,9 @@ class TestVisualization(unittest.TestCase):
         image = svg_dendrogram(dendrogram, names=np.arange(n), width=200, height=200, margin=10, margin_text=5, scale=3,
                                n_clusters=3, color='green', font_size=14, reorder=True, rotate=True)
         self.assertEqual(image[1:4], 'svg')
+        image = svg_dendrogram(dendrogram, names=np.arange(n), width=200, height=200, margin=10, margin_text=5, scale=3,
+                               n_clusters=3, color='green', font_size=14, reorder=False, rotate=True)
+        self.assertEqual(image[1:4], 'svg')
 
     def test_directed(self):
         graph = painters(True)
