3246ddb7bf7ac2b790f250a8987f34a6bdd1b841
Unknown <nathan.de-lara@polytechnique.edu>
2020-04-18 00:07:52 +0200

    Specific file for basic routines

---
+++ sknetwork/linalg/basics.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Apr 2020
+@author: Nathan de Lara <ndelara@enst.fr>
+"""
+
+import numpy as np
+from scipy import sparse, linalg
+
+from sknetwork.linalg.sparse_lowrank import SparseLR
+
+
+def safe_sparse_dot(a, b):
+    """Dot product that handles the sparse matrix case correctly.
+    Uses BLAS GEMM as replacement for numpy.dot where possible to avoid unnecessary copies.
+
+    Parameters
+    ----------
+    a : array or sparse matrix or SparseLR
+    b : array or sparse matrix or SparseLR
+    Returns
+    -------
+    dot_product : array or sparse matrix
+        sparse if ``a`` or ``b`` is sparse.
+    """
+    if type(a) == SparseLR and type(b) == np.ndarray:
+        return a.dot(b)
+    if type(b) == SparseLR and type(a) == np.ndarray:
+        return b.T.dot(a.T).T
+    if type(a) == SparseLR and type(b) == SparseLR:
+        raise NotImplementedError
+    if type(a) == SparseLR and type(b) == sparse.csr_matrix:
+        return a.right_sparse_dot(b)
+    if type(b) == SparseLR and type(a) == sparse.csr_matrix:
+        return b.left_sparse_dot(a)
+    if type(a) == np.ndarray:
+        return b.T.dot(a.T).T
+    return a.dot(b)
--- sknetwork/linalg/randomized_methods.py
+++ sknetwork/linalg/randomized_methods.py
@@ -11,36 +11,9 @@ from typing import Union, Tuple
 import numpy as np
 from scipy import sparse, linalg
 
+from sknetwork.linalg.basics import safe_sparse_dot
 from sknetwork.linalg.sparse_lowrank import SparseLR
 from sknetwork.utils.check import check_random_state, check_square
-
-
-def safe_sparse_dot(a, b):
-    """Dot product that handles the sparse matrix case correctly.
-    Uses BLAS GEMM as replacement for numpy.dot where possible to avoid unnecessary copies.
-
-    Parameters
-    ----------
-    a : array or sparse matrix or SparseLR
-    b : array or sparse matrix or SparseLR
-    Returns
-    -------
-    dot_product : array or sparse matrix
-        sparse if ``a`` or ``b`` is sparse.
-    """
-    if type(a) == SparseLR and type(b) == np.ndarray:
-        return a.dot(b)
-    if type(b) == SparseLR and type(a) == np.ndarray:
-        return b.T.dot(a.T).T
-    if type(a) == SparseLR and type(b) == SparseLR:
-        raise NotImplementedError
-    if type(a) == SparseLR and type(b) == sparse.csr_matrix:
-        return a.right_sparse_dot(b)
-    if type(b) == SparseLR and type(a) == sparse.csr_matrix:
-        return b.left_sparse_dot(a)
-    if type(a) == np.ndarray:
-        return b.T.dot(a.T).T
-    return a.dot(b)
 
 
 def randomized_range_finder(matrix: np.ndarray, size: int, n_iter: int, power_iteration_normalizer='auto',
