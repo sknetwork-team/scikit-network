2d8aa49acabf52e6242e7ffa2a85560e6322760e
Unknown <nathan.de-lara@polytechnique.edu>
2020-05-18 20:53:28 +0200

    Fix stopping criterion for diteration

--- sknetwork/linalg/diteration.pyx
+++ sknetwork/linalg/diteration.pyx
@@ -16,10 +16,14 @@ def diffusion(int[:] indptr, int[:] indices, float[:] data, float[:] scores, flo
     cdef int n = fluid.shape[0]
     cdef int i
     cdef int j
+    cdef int j1
+    cdef int j2
     cdef int jj
     cdef float sent
     cdef float tmp
-    cdef float residu = 1
+    cdef float removed
+    cdef float restart_prob = 1 - damping_factor
+    cdef float residu = restart_prob
 
     for k in range(n_iter):
         for i in prange(n, nogil=True, schedule='guided'):
@@ -27,11 +31,17 @@ def diffusion(int[:] indptr, int[:] indices, float[:] data, float[:] scores, flo
             if sent > 0:
                 scores[i] += sent
                 fluid[i] = 0
+                j1 = indptr[i]
+                j2 = indptr[i+1]
                 tmp = sent * damping_factor
-                for jj in range(indptr[i], indptr[i+1]):
-                    j = indices[jj]
-                    fluid[j] += tmp * data[jj]
-                residu -= sent
-        if residu < tol:
+                if j2 != j1:
+                    for jj in range(j1, j2):
+                        j = indices[jj]
+                        fluid[j] += tmp * data[jj]
+                    removed = sent * restart_prob
+                else:
+                    removed = sent
+                residu -= removed
+        if residu < tol * restart_prob:
             return
     return
