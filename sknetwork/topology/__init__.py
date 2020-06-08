877bc8b0e235a64a2b2a64ec8025ec5fb74afb96
Sim <julien.simonnet@etu.upmc.fr>
2020-06-08 19:28:36 +0200

    Adds a triangle listing algorithm in Cython.

--- sknetwork/topology/__init__.py
+++ sknetwork/topology/__init__.py
@@ -1,2 +1,3 @@
 """path module"""
 from sknetwork.topology.structure import is_bipartite, largest_connected_component, connected_components
+from sknetwork.topology.triangles import TriangleListing
---
+++ sknetwork/topology/tests/test_triangles.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Tests for label propagation"""
+
+import unittest
+
+from sknetwork.topology import TriangleListing
+from sknetwork.data.test_graphs import *
+from sknetwork.data import karate_club
+
+from scipy.special import comb
+
+class TestTriangleListing(unittest.TestCase):
+	
+	def test_empty(self):
+		adjacency = test_graph_empty()
+		n = adjacency.shape[0]
+		nb = TriangleListing().fit_transform(adjacency)
+		self.assertEqual(nb, 0)
+	
+	def test_disconnected(self):
+		adjacency = test_graph_disconnect()
+		n = adjacency.shape[0]
+		nb = TriangleListing().fit_transform(adjacency)
+		self.assertEqual(nb, 1)
+	
+	def test_cliques(self):
+		adjacency = test_graph_clique()
+		n = adjacency.shape[0]
+		nb = TriangleListing().fit_transform(adjacency)
+		self.assertEqual(nb, comb(n, 3))
+		
+	def test_options(self):
+		adjacency = karate_club()
+		
+		tri = TriangleListing()
+		nb = tri.fit_transform(adjacency)
+		self.assertEqual(nb, 45)
+		
+		tri = TriangleListing(parallelize=True)
+		nb = tri.fit_transform(adjacency)
+		self.assertEqual(nb, 45)
+		
+		"""
+		lab = TriangleListing(printing=True)
+		nb = tri.fit_transform(adjacency)
+		self.assertEqual(nb, 45)
+		"""
