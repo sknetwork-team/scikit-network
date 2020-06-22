e06316c64df3e3adfffc762011608f4df9e5f416
Victor Manach <victor.manach@telecom-paris.fr>
2020-06-22 11:01:57 +0200

    Add __init__ function to Force Atlas class

--- sknetwork/embedding/force_atlas.py
+++ sknetwork/embedding/force_atlas.py
@@ -5,3 +5,15 @@ Created on Jun 2020
 @author: Victor Manach <victor.manach@telecom-paris.fr>
 @author: Rémi Jaylet <remi.jaylet@telecom-paris.fr>
 """
+
+from sknetwork.embedding.base import BaseEmbedding
+
+class Force_atlas(BaseEmbedding):
+
+    def __init__(self, strength: float = None, n_iter: int = 50, tol: float = 1e-4):
+        super(Force_atlas, self).__init__()
+        self.strength = strength
+        self.n_iter = n_iter
+        self.tol = tol
+
+    
