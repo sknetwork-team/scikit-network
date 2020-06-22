17dfac98812280e298148e359a7c236ec5a7bf80
Pierre <pierre.pebereau@telecom-paris.fr>
2020-06-22 18:59:37 +0200

    Unoptimized yet functional cythonization of the WL coloring algorithm

--- sknetwork/topology/wl_coloring.pyx
+++ sknetwork/topology/wl_coloring.pyx
@@ -18,32 +18,31 @@ from sknetwork.utils.base import Algorithm
 
 cimport cython
 
-cdef void c_wl_coloring(int[:] indices, int[:] indptr, int[:]  labels) :
+cdef int[:] c_wl_coloring(int[:] indices, int[:] indptr) :
     cdef int n = indptr.shape[0] - 1
 
     # labels_i denotes the array of the labels at the i-th iteration.
     # labels_i_1 denotes the array of the labels at the i-1-th iteration.
     DTYPE = np.int32
+    labels = np.zeros(n, dtype = np.int32)
     cdef int[:]  labels_i = np.ones(n, dtype = DTYPE)
     cdef int[:]  labels_i_1 = np.zeros(n, dtype = DTYPE)
 
    # cdef int[:]  degres = np.array(indptr[1:]) - np.array(indptr[:-1])
 
-    cdef int[:,:]  multiset = np.zeros((n,n), dtype = DTYPE)
-
-
+    cdef np.ndarray multiset = np.zeros((n,n), dtype = DTYPE)
 
     long_label = [ (0,0) for _ in range(n)]
 
 
     cdef int i = 1
     cdef int u = 0
-
+    cdef int current_max = 0
+    cdef j = 0
     while i < n and (np.array(labels_i_1) != np.array(labels_i)).any() :
 
         labels_i_1 = np.copy(labels_i) #Perf : ne pas utiliser copy? echanger les addresses ?
         labels_i = np.zeros(n, dtype = DTYPE)
-
 
         for v in range(n):
             # 1
@@ -54,14 +53,13 @@ cdef void c_wl_coloring(int[:] indices, int[:] indptr, int[:]  labels) :
                 j+=1
 
             # 2
-            multiset[v]=np.array(multiset[v]).sort() #TODO Tri par paquet
-
-            long_label[v] = (int( str(labels_i_1[v]) + "".join(multiset[v])), v) #Efficace ?
-
+            multiset[v] =  (np.sort(multiset[v]))
+            temp_string = str(labels_i_1[v])
+            for num in multiset[v] :
+                temp_string+=str(num)
+            long_label[v] = (temp_string, v) #Efficace ?
         # 3
-
         long_label.sort(key=lambda x: x[0])  # sort along first axis
-
         new_hash = {}
         current_max = 0
         for (long_label_v, v) in long_label:
@@ -71,13 +69,11 @@ cdef void c_wl_coloring(int[:] indices, int[:] indptr, int[:]  labels) :
             #  4
 
             labels_i[int(v)] = new_hash[long_label_v]
-
         i += 1
 
     labels = labels_i
-    print(np.array(labels))
-    print(i)
-    return
+
+    return labels
 
 
 
@@ -142,7 +138,7 @@ class WLColoring(Algorithm):
 
         self.labels_ = np.zeros(n, dtype = np.int32)
 
-        c_wl_coloring(indices,  indptr, self.labels_)
+        self.labels_ = c_wl_coloring(indices,  indptr)
 
 
         return self
