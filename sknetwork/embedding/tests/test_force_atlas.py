7e9d4726f07add4f632f6309c80213e1ea1daf3e
Victor Manach <victor.manach@telecom-paris.fr>
2020-06-23 09:20:19 +0200

    Change way of importing force atlas in test file

--- sknetwork/embedding/tests/test_force_atlas.py
+++ sknetwork/embedding/tests/test_force_atlas.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from sknetwork.embedding import force_atlas
+from sknetwork.embedding.force_atlas import ForceAtlas2
 from sknetwork.data.test_graphs import *
 
 
@@ -13,6 +13,6 @@ class TestEmbeddings(unittest.TestCase):
     def test_shape(self):
         for adjacency in [test_graph(), test_digraph()]:
             n = adjacency.shape[0]
-            force_atlas_graph = force_atlas.ForceAtlas2()
-            layout = force_atlas_graph.fit_transform(adjacency)
+            force_atlas = ForceAtlas2()
+            layout = force_atlas.fit_transform(adjacency)
             self.assertEqual((n, 2), layout.shape)
