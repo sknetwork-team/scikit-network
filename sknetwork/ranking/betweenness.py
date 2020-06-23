9d7e14f5a5d0df92c50a94ca3f339fbb28065f98
mynameisvinn <vin.tang@gmail.com>
2020-06-23 19:45:06 -0400

    merge fit method with brandes, rename variables

--- sknetwork/ranking/betweenness.py
+++ sknetwork/ranking/betweenness.py
@@ -1,8 +1,9 @@
-from collections import deque
 from typing import Union
+from collections import deque
 
 import numpy as np
 from scipy import sparse
+from scipy.sparse.linalg import LinearOperator
 
 from sknetwork.ranking.base import BaseRanking
 from sknetwork.utils.check import check_format
@@ -11,16 +12,10 @@ from sknetwork.utils.check import check_format
 class Betweenness(BaseRanking):
     """Betweenness centrality:
 
-    Examples
-    --------
-    >>> from sknetwork.data.toy_graphs import house
-    >>> adjacency = house()
-    >>> betweeness = Betweenness()
-    >>> scores = betweeness.fit_transform(adjacency)
-    array([0., 3., 1., 1., 3.])
-
-    References
-    ----------
+    compute betweenness centrality given adjacency matrix of graph.
+    
+    Reference
+    ---------
     Brandes, U. (2001). `A FasterAlgorithmforBetweennessCentrality 
     <https://www.eecs.wsu.edu/~assefaw/CptS580-06/papers/brandes01centrality.pdf>`_. 
     Journalof Mathematical Sociology 25(2), 163-177.
@@ -29,40 +24,46 @@ class Betweenness(BaseRanking):
         super(Betweenness, self).__init__()
 
     def fit(self, adjacency: Union[sparse.csr_matrix, np.ndarray]) -> 'Betweenness':
-        self.scores_ = _brandes(adjacency)
+        """Betweeness centrality.
+
+        Parameters
+        ----------
+        adjacency :
+            Adjacency matrix of the graph.
+        
+        Returns
+        -------
+        self: :class:`Betweenness`
+        """
+        if not isinstance(adjacency, LinearOperator):
+            adjacency = check_format(adjacency)
+        vertices = np.arange(adjacency._shape[0])
+        betweenness = dict((vertex, 0) for vertex in vertices)
+        for vertex in vertices:
+            stack = []
+            P = dict((w, []) for w in vertices)
+            sigma = dict((t, 0) for t in vertices); sigma[vertex] = 1
+            d = dict((t, -1) for t in vertices); d[vertex] = 0
+            queue = deque([])
+            queue.append(vertex)
+
+            while queue:
+                v = queue.popleft()
+                stack.append(v)
+                for neighbor in adjacency[v].indices:
+                    if d[neighbor] < 0:
+                        queue.append(neighbor)
+                        d[neighbor] = d[v] + 1
+                    if d[neighbor] == d[v] + 1:
+                        sigma[neighbor] = sigma[neighbor] + sigma[v]
+                        P[neighbor].append(v)
+            delta = dict((vertex, 0) for vertex in vertices)
+            
+            while stack:
+                w = stack.pop()
+                for v in P[w]:
+                    delta[v] = delta[v] + (sigma[v] / sigma[w]) * (1 + delta[w])
+                    if w != vertex:
+                        betweenness[w] = betweenness[w] + delta[w]
+        self.scores_ = np.array([betweenness[k] for k in sorted(betweenness.keys())])
         return self
-
-
-def _brandes(A):
-    """compute betweenness centrality given adjacency matrix of graph.
-
-    https://www.eecs.wsu.edu/~assefaw/CptS580-06/papers/brandes01centrality.pdf
-    """
-    V = np.arange(A._shape[0])
-    
-    C = dict((v, 0) for v in V)
-    for s in V:
-        S = []
-        P = dict((w, []) for w in V)
-        g = dict((t, 0) for t in V); g[s] = 1
-        d = dict((t, -1) for t in V); d[s] = 0
-        Q = deque([])
-        Q.append(s)
-        while Q:
-            v = Q.popleft()
-            S.append(v)
-            for w in A[v].indices:
-                if d[w] < 0:
-                    Q.append(w)
-                    d[w] = d[v] + 1
-                if d[w] == d[v] + 1:
-                    g[w] = g[w] + g[v]
-                    P[w].append(v)
-        e = dict((v, 0) for v in V)
-        while S:
-            w = S.pop()
-            for v in P[w]:
-                e[v] = e[v] + (g[v] / g[w]) * (1 + e[w])
-                if w != s:
-                    C[w] = C[w] + e[w]
-    return np.array([C[k] for k in sorted(C.keys())])
