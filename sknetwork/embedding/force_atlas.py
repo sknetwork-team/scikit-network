5c08858c7b002129835d45e542fbd70311c535d1
remij <remi.jaylet@hotmail.com>
2020-07-01 10:09:55 +0200

    Minor changes in force_apply

--- sknetwork/embedding/force_atlas.py
+++ sknetwork/embedding/force_atlas.py
@@ -265,6 +265,7 @@ class Cell:
         self.n_particles = 0
         self.pos_particle = None
         self.particle_degree = None
+        self.cell_size = 0
 
     def is_in_cell(self, position: np.ndarray) -> bool:  # test if a particle is inside the cell's bounds
         return (self.pos_min <= position).all() and (position <= self.pos_max).all()
@@ -280,6 +281,7 @@ class Cell:
                 self.pos_particle = None
             for child in self.children:
                 child.add(position, degree)
+                self.cell_size = self.pos_max[0] - self.pos_min[0]
         else:
             self.pos_particle = position
             self.particle_degree = degree
@@ -298,24 +300,22 @@ class Cell:
         self.children = np.asarray([child_1, child_2, child_3, child_4])
 
     def apply_force(self, pos_node, node_degree, theta, repulsion, repulsive_factor: float):
-        if self.n_particles == 0:
-            return
-        cell_size = self.pos_max[0] - self.pos_min[0]
-        grad: np.ndarray = pos_node - self.center
+
         if self.n_particles == 1:  # compute repulsion force between two nodes
             variation = self.pos_particle - pos_node
             distance = np.linalg.norm(variation, axis=0)
             if distance > 0:
                 repulsion_force = repulsive_factor * node_degree * (self.n_particles + 1) * variation / distance
                 repulsion += repulsion_force
-        else:
+
+        elif self.n_particles > 1:
+            grad: np.ndarray = pos_node - self.center
             distance = np.linalg.norm(grad, axis=0)
-            if distance * theta > cell_size:
+            if distance * theta > self.cell_size:
                 repulsion_force = repulsive_factor * node_degree * (self.n_particles + 1) * grad / distance
                 repulsion += repulsion_force
 
             else:
                 for sub_cell in self.children:
-                    if sub_cell.n_particles > 0:
-                        sub_cell.apply_force(pos_node, node_degree, theta, repulsion, repulsive_factor)
+                    sub_cell.apply_force(pos_node, node_degree, theta, repulsion, repulsive_factor)
         return repulsion
