75fa61655c3973f49b50f1ff8b9e7b7d9d7df3c1
Pierre <pierre.pebereau@telecom-paris.fr>
2020-07-02 18:20:41 +0200

    Correcting test cases

--- sknetwork/topology/tests/test_wl_coloring.py
+++ sknetwork/topology/tests/test_wl_coloring.py
@@ -12,22 +12,22 @@ class TestWLColoring(unittest.TestCase):
 
     def test_empty(self):
         adjacency = test_graph_empty()
-        labels = WLColoring().fit_transform(-1, adjacency)
+        labels = WLColoring().fit_transform(adjacency)
         self.assertTrue((labels == np.zeros(10)).all())
 
     def test_cliques(self):
         adjacency = test_graph_clique()
-        labels = WLColoring().fit_transform(-1, adjacency)
+        labels = WLColoring().fit_transform(adjacency)
         self.assertTrue((labels == np.zeros(10)).all())
 
     def test_house(self):
         adjacency = house()
-        labels = WLColoring().fit_transform(-1, adjacency)
+        labels = WLColoring().fit_transform(adjacency)
         self.assertTrue((labels == np.array([2, 0, 1, 1, 0])).all())
 
     def test_bow_tie(self):
         adjacency = bow_tie()
-        labels = WLColoring().fit_transform(-1, adjacency)
+        labels = WLColoring().fit_transform(adjacency)
         self.assertTrue((labels == np.array([0, 1, 1, 1, 1])).all())
 
     def test_iso(self):
@@ -36,8 +36,8 @@ class TestWLColoring(unittest.TestCase):
         reorder = list(range(n))
         np.random.shuffle(reorder)
         adjacency2 = adjacency[reorder][:, reorder]
-        l1 = WLColoring().fit_transform(-1, adjacency)
-        l2 = WLColoring().fit_transform(-1, adjacency2)
+        l1 = WLColoring().fit_transform(adjacency)
+        l2 = WLColoring().fit_transform(adjacency2)
         l1.sort()
         l2.sort()
         self.assertTrue((l1 == l2).all())
