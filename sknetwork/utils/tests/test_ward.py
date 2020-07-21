cb2d372583f8f5d9b8f36b6bc4b4fe1731e7bafe
Unknown <nathan.de-lara@polytechnique.edu>
2020-07-21 10:15:14 +0200

    Add "fit_transform" method
    * KMeans
    * Ward

--- sknetwork/utils/tests/test_ward.py
+++ sknetwork/utils/tests/test_ward.py
@@ -4,7 +4,6 @@
 Created on October 2019
 @author: Nathan de Lara <ndelara@enst.fr>
 """
-
 import unittest
 
 import numpy as np
@@ -15,7 +14,7 @@ from sknetwork.utils import WardDense
 class TestKMeans(unittest.TestCase):
 
     def test_kmeans(self):
-        self.x = np.random.randn(10, 3)
+        x = np.random.randn(10, 3)
         ward = WardDense()
-        ward.fit(self.x)
-        self.assertEqual(ward.dendrogram_.shape, (self.x.shape[0] - 1, 4))
+        dendrogram = ward.fit_transform(x)
+        self.assertEqual(dendrogram.shape, (x.shape[0] - 1, 4))
