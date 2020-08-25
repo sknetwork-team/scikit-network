32777d44e7300a77ba0490dfa452f3e0b891e6cb
Unknown <nathan.de-lara@polytechnique.edu>
2020-08-25 18:20:45 +0200

    Coverage
    Linkpred

--- sknetwork/linkpred/tests/test_API.py
+++ sknetwork/linkpred/tests/test_API.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""tests for link prediction APi"""
+"""tests for link prediction API"""
+from abc import ABC
+from typing import Iterable
 import unittest
 
 import numpy as np
 
 from sknetwork.data import house
-from sknetwork.linkpred import CommonNeighbors
 from sknetwork.linkpred.base import BaseLinkPred
+
+
+class Dummy(BaseLinkPred, ABC):
+    """Dummy algorithm for testing purposes"""
+    def __init__(self):
+        super(Dummy, self).__init__()
+
+    def fit(self, *args, **kwargs):
+        """Dummy fit method"""
+        return self
+
+    def _predict_base(self, source: int, targets: Iterable):
+        return np.array([1 for _ in targets])
 
 
 class TestLinkPred(unittest.TestCase):
@@ -23,10 +37,13 @@ class TestLinkPred(unittest.TestCase):
 
     def test_query(self):
         adjacency = house()
-        algo = CommonNeighbors().fit(adjacency)
+        algo = Dummy().fit(adjacency)
         query = 'toto'
         with self.assertRaises(ValueError):
             algo.predict(query)
         query = np.ones((2, 3))
         with self.assertRaises(ValueError):
             algo.predict(query)
+        query = [(0, 1), (2, 3)]
+        pred = algo.predict(query)
+        self.assertEqual(pred.shape, (len(query),))
