6bb89a267c1e5526a99cba9fac34a39d9191e664
Tiphaine Viard <tiphaine.viard@telecom-paris.fr>
2020-10-14 18:08:42 +0200

    Added test to check validity on bigraphs

--- sknetwork/ranking/tests/test_betweenness.py
+++ sknetwork/ranking/tests/test_betweenness.py
@@ -3,10 +3,11 @@
 """tests for betweenness.py"""
 
 import unittest
+import numpy as np
 
 from sknetwork.ranking.betweenness import Betweenness
-from sknetwork.data.test_graphs import *
-from sknetwork.data.toy_graphs import bow_tie
+from sknetwork.data.test_graphs import test_graph, test_graph_disconnect
+from sknetwork.data.toy_graphs import bow_tie, star_wars
 
 
 class TestBetweenness(unittest.TestCase):
@@ -28,3 +29,10 @@ class TestBetweenness(unittest.TestCase):
         betweenness = Betweenness()
         with self.assertRaises(ValueError):
             betweenness.fit(adjacency)
+
+    def test_bipartite(self):
+        adjacency = star_wars()
+        betweenness = Betweenness()
+
+        with self.assertRaises(ValueError):
+            betweenness.fit_transform(adjacency)
