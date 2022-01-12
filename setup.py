6eda515e56a95f7a90623129c67acd6bfd17a6ad
Thomas Bonald <thomas.bonald@telecom-paristech.fr>
2022-01-12 14:36:52 +0100

    requirements numpy / scipy

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from distutils.command.build_ext import build_ext
 import os
 from glob import glob
 
-dist.Distribution().fetch_build_eggs(['Cython', 'numpy==1.20.0'])
+dist.Distribution().fetch_build_eggs(['Cython', 'numpy==1.22.0'])
 
 import numpy
 
@@ -21,7 +21,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy>=1.20.2', 'scipy>=1.6.2']
+requirements = ['numpy>=1.22.0', 'scipy>=1.7.3']
 
 setup_requirements = ['pytest-runner']
 
