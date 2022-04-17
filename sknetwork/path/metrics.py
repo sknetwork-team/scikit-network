781e274047a85db4e3b7cb7d6eb0db73ef2cc874
hcars <hcarscad@gmail.com>
2022-04-16 22:49:04 -0700

    Added radius and eccentricity methods with tests.

--- sknetwork/path/metrics.py
+++ sknetwork/path/metrics.py
@@ -9,10 +9,10 @@ from typing import Union, Optional
 import numpy as np
 from scipy import sparse
 
-from sknetwork.path.shortest_path import distance
+from sknetwork.path.shortest_path import distance, shortest_path
 
 
-def diameter(adjacency: Union[sparse.csr_matrix, np.ndarray], n_sources: Optional[Union[int, float]] = None,
+def diameter(adjacency: Union[sparse.csr_matrix, np.ndarray], n_sources: Optional[Union[int, float]] = None, unweighted: bool = False,
              n_jobs: Optional[int] = None) -> int:
     """Lower bound on the diameter of a graph which is the length of the longest shortest path between two nodes.
 
@@ -26,6 +26,8 @@ def diameter(adjacency: Union[sparse.csr_matrix, np.ndarray], n_sources: Optiona
         * If None, compute exact diameter.
         * If int, sample n_sample source nodes at random.
         * If float, sample (n_samples * n) source nodes at random.
+    unweighted:
+        Whether or not the graph is unweighted.
     n_jobs :
         If an integer value is given, denotes the number of workers to use (-1 means the maximum number will be used).
         If ``None``, no parallel computations are made.
@@ -63,5 +65,72 @@ def diameter(adjacency: Union[sparse.csr_matrix, np.ndarray], n_sources: Optiona
         else:
             raise ValueError("n_sources must be either None, an integer smaller than the number of nodes or a float"
                              "smaller than 1.")
-    dists = distance(adjacency, sources, method='D', return_predecessors=False, n_jobs=n_jobs).astype(int)
+    dists = distance(adjacency, sources, method='D', return_predecessors=False, unweighted=unweighted, n_jobs=n_jobs).astype(int)
+    return dists.max()
+
+
+def radius(adjacency: Union[sparse.csr_matrix, np.ndarray],
+            unweighted: bool = False, n_jobs: Optional[int] = None) -> int:
+    """Computes the radius of the graph which. The radius of the graph is the minimum eccentricity of the graph.
+
+    Parameters
+    ----------
+    adjacency :
+        Adjacency matrix of the graph.
+    unweighted:
+        Whether the graph is unweighted.
+    n_jobs :
+        If an integer value is given, denotes the number of workers to use (-1 means the maximum number will be used).
+        If ``None``, no parallel computations are made.
+
+    Returns
+    -------
+    radius : int
+
+    Examples
+    --------
+
+
+    Notes
+    -----
+    This is a basic implementation that computes distances between nodes and returns the maximum.
+    """
+    # Get the nodes.
+    n = adjacency.shape[0]
+    nodes = np.arange(n)
+    # Get the eccentricities of each node.
+    eccentricities = map(lambda x: eccentricity(adjacency=adjacency, node=x, unweighted=unweighted, n_jobs=n_jobs), nodes)
+
+    return min(eccentricities)
+
+
+
+def eccentricity(adjacency: Union[sparse.csr_matrix, np.ndarray], node: int,
+             unweighted: bool = False, n_jobs: Optional[int] = None) -> int:
+    """Computes the eccentricity of a node. The eccentricity of a node, u, is the
+    maximum length of the shortest paths from u to the other nodes in the graph.
+
+    Parameters
+    ----------
+    adjacency :
+        Adjacency matrix of the graph.
+    node:
+        The node to compute the eccentricity for.
+    unweighted:
+        Whether or not the graph is unweighted.
+    n_jobs :
+        If an integer value is given, denotes the number of workers to use (-1 means the maximum number will be used).
+        If ``None``, no parallel computations are made.
+
+    Returns
+    -------
+    eccentricity : int
+
+    Examples
+    --------
+
+
+    """
+
+    dists = distance(adjacency, node, method='D', return_predecessors=False, unweighted=unweighted, n_jobs=n_jobs).astype(int)
     return dists.max()
--- sknetwork/path/tests/test_metrics.py
+++ sknetwork/path/tests/test_metrics.py
@@ -4,12 +4,21 @@
 import unittest
 
 from sknetwork.data import house
-from sknetwork.path import diameter
+from sknetwork.path import diameter, eccentricity, radius
 
 
 class TestMetrics(unittest.TestCase):
 
-    def test_diameter(self):
+    def test_diameter_1(self):
         adjacency = house()
         with self.assertRaises(ValueError):
             diameter(adjacency, 2.5)
+    def test_diameter_2(self):
+        adjacency = house()
+        self.assertEqual(diameter(adjacency), 2)
+    def test_eccentricity_1(self):
+        adjacency = house()
+        self.assertEqual(eccentricity(adjacency, 1), 2)
+    def test_radius_1(self):
+        adjacency = house()
+        self.assertEqual(radius(adjacency), 2)
