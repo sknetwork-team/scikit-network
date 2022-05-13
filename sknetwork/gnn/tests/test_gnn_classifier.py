9694148f4c63770940e40d1b085fd4fc2fcc3aa9
simondelarue <sim.delarue@gmail.com>
2022-05-13 12:56:36 +0200

    updated tests with mask related parameters

--- sknetwork/gnn/tests/test_gnn_classifier.py
+++ sknetwork/gnn/tests/test_gnn_classifier.py
@@ -15,51 +15,64 @@ class TestGNNClassifier(unittest.TestCase):
     def setUp(self) -> None:
         """Test graph for tests."""
         self.adjacency = test_graph()
+        self.n = self.adjacency.shape[0]
         self.features = self.adjacency
         self.labels = np.array([0]*5 + [1]*5)
 
     def test_gnn_classifier_sparse_feat(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 2)
         self.assertTrue(gnn.conv2.activation == 'softmax')
-        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels)
+        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels, test_size=0.2)
         embedding = gnn.conv2.emb
-        self.assertTrue(len(y_pred) == self.adjacency.shape[0])
-        self.assertTrue(embedding.shape == (self.adjacency.shape[0], 2))
+        self.assertTrue(len(y_pred) == self.n)
+        self.assertTrue(embedding.shape == (self.n, 2))
 
     def test_gnn_classifier_dense_feat(self):
         # features not in nparray
         features = self.adjacency.todense()
         gnn = GNNClassifier(features.shape[1], 4, 2)
         with self.assertRaises(TypeError):
-            gnn.fit_transform(self.adjacency, features, self.labels)
+            gnn.fit_transform(self.adjacency, features, self.labels, test_size=0.2)
 
         # features in nparray
         features = np.array(self.adjacency.todense())
         gnn = GNNClassifier(features.shape[1], 4, 2)
-        y_pred = gnn.fit_transform(self.adjacency, features, self.labels)
+        y_pred = gnn.fit_transform(self.adjacency, features, self.labels, test_size=0.2)
         embedding = gnn.conv2.emb
-        self.assertTrue(len(y_pred) == self.adjacency.shape[0])
-        self.assertTrue(embedding.shape == (self.adjacency.shape[0], 2))
+        self.assertTrue(len(y_pred) == self.n)
+        self.assertTrue(embedding.shape == (self.n, 2))
 
     def test_gnn_classifier_optimizer(self):
         opts = ['none', 'Adam']
         for opt in opts:
             gnn = GNNClassifier(self.features.shape[1], 4, 2, opt=opt)
-            y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels)
+            y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels, test_size=0.2)
             embedding = gnn.conv2.emb
-            self.assertTrue(len(y_pred) == self.adjacency.shape[0])
-            self.assertTrue(embedding.shape == (self.adjacency.shape[0], 2))
+            self.assertTrue(len(y_pred) == self.n)
+            self.assertTrue(embedding.shape == (self.n, 2))
 
     def test_gnn_classifier_loss(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 2)
         with self.assertRaises(ValueError):
-            gnn.fit_transform(self.adjacency, self.features, self.labels, loss='toto')
+            gnn.fit_transform(self.adjacency, self.features, self.labels, loss='toto', test_size=0.2)
 
     def test_gnn_classifier_1label(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 1)
-        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels)
-        self.assertTrue(len(y_pred) == self.adjacency.shape[0])
+        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels, test_size=0.2)
+        self.assertTrue(len(y_pred) == self.n)
         self.assertTrue(np.min(y_pred) >= 0 and np.max(y_pred) <= 1)
+
+    def test_gnn_classifier_masks(self):
+        gnn = GNNClassifier(self.features.shape[1], 4, 2)
+        with self.assertRaises(ValueError):
+            gnn.fit_transform(self.adjacency, self.features, self.labels, train_mask=np.random.randint(2, size=self.n))
+        with self.assertRaises(ValueError):
+            gnn.fit_transform(self.adjacency, self.features, self.labels, test_mask=np.random.randint(2, size=self.n))
+        train_mask = np.random.randint(2, size=self.n).astype(bool)
+        test_mask = np.invert(train_mask)
+        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels, train_mask=train_mask,
+                                   test_mask=test_mask)
+        self.assertTrue(len(y_pred) == self.n)
 
     def test_gnn_classifier_test_size(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 2)
@@ -70,21 +83,21 @@ class TestGNNClassifier(unittest.TestCase):
 
     def test_gnn_classifier_shuffle(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 2)
-        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels, shuffle=False)
+        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels, test_size=0.2, shuffle=False)
         embedding = gnn.conv2.emb
-        self.assertTrue(len(y_pred) == self.adjacency.shape[0])
-        self.assertTrue(embedding.shape == (self.adjacency.shape[0], 2))
+        self.assertTrue(len(y_pred) == self.n)
+        self.assertTrue(embedding.shape == (self.n, 2))
 
     def test_gnn_classifier_random_state(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 2)
-        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels, random_state=42)
+        y_pred = gnn.fit_transform(self.adjacency, self.features, self.labels, test_size=0.2, random_state=42)
         embedding = gnn.conv2.emb
         self.assertTrue(len(y_pred) == self.adjacency.shape[0])
         self.assertTrue(embedding.shape == (self.adjacency.shape[0], 2))
 
     def test_gnn_classifier_predict(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 2)
-        _ = gnn.fit_transform(self.adjacency, self.features, self.labels, random_state=42)
+        _ = gnn.fit_transform(self.adjacency, self.features, self.labels, test_size=0.2, random_state=42)
 
         # test result shape for one new node
         new_n = np.random.randint(2, size=self.features.shape[1])
--- sknetwork/gnn/tests/test_optimizer.py
+++ sknetwork/gnn/tests/test_optimizer.py
@@ -25,7 +25,7 @@ class TestOptimizer(unittest.TestCase):
 
     def test_optimizer_adam(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 2, opt='Adam')
-        _ = gnn.fit_transform(self.adjacency, self.features, self.labels, max_iter=1)
+        _ = gnn.fit_transform(self.adjacency, self.features, self.labels, max_iter=1, test_size=0.2)
         conv1_W, conv2_W = gnn.conv1.W.copy(), gnn.conv2.W.copy()
         conv1_b, conv2_b = gnn.conv1.bias.copy(), gnn.conv2.bias.copy()
         gnn.opt.step()
@@ -42,7 +42,7 @@ class TestOptimizer(unittest.TestCase):
 
     def test_optimizer_gd(self):
         gnn = GNNClassifier(self.features.shape[1], 4, 2, opt='none')
-        _ = gnn.fit_transform(self.adjacency, self.features, self.labels, max_iter=1)
+        _ = gnn.fit_transform(self.adjacency, self.features, self.labels, max_iter=1, test_size=0.2)
         conv1_W, conv2_W = gnn.conv1.W.copy(), gnn.conv2.W.copy()
         conv1_b, conv2_b = gnn.conv1.bias.copy(), gnn.conv2.bias.copy()
         gnn.opt.step()
