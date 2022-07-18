7e7e61aee5e512bb00b70db990f3f6b4510d0435
hcars <hcarscad@gmail.com>
2022-07-17 19:45:28 -0700

    Fixed max flow output.

--- sknetwork/flow/tests/test_flow.py
+++ sknetwork/flow/tests/test_flow.py
@@ -8,9 +8,9 @@ from scipy import sparse
 
 class TestFlow(unittest.TestCase):
     def test_push_relabel_1(self):
-        adj = sparse.csr_matrix([[0, 2, 3, 0, 0, 0, 0], [2, 0, 0, 3, 0, 0, 0],
-        [3, 0, 0, 2, 0, 0, 0], [0, 3, 0, 0, 1, 3, 0], [0, 0, 0, 1, 0, 0, 2],
-        [0, 0, 0, 3, 0, 0, 2], [0, 0, 0, 0, 0, 0, 0]])
+        adj = sparse.csr_matrix([[0, 2, 3, 0, 0, 0, 0], [0, 0, 0, 3, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 1, 3, 0], [0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 2], [0, 0, 0, 0, 0, 0, 0]])
         flow = push_relabel(adj, 0, 6)
         self.assertEqual(3, flow[:, 6].sum())
         for i in range(1, 6):
