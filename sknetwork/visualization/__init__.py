3ccf3fdc62ecb68ba04175e0e7603c8319bb7296
Thomas Bonald <thomas.bonald@telecom-paristech.fr>
2022-07-26 16:52:31 +0200

    Remove svg_digraph -> svg_graph

--- sknetwork/visualization/__init__.py
+++ sknetwork/visualization/__init__.py
@@ -1,4 +1,4 @@
 """Visualization module."""
 
 from sknetwork.visualization.dendrograms import svg_dendrogram
-from sknetwork.visualization.graphs import svg_graph, svg_bigraph, svg_digraph
+from sknetwork.visualization.graphs import svg_graph, svg_bigraph
--- sknetwork/visualization/tests/test_graphs.py
+++ sknetwork/visualization/tests/test_graphs.py
@@ -9,7 +9,7 @@ from scipy import sparse
 
 from sknetwork.data.test_graphs import test_graph_disconnect, test_bigraph_disconnect
 from sknetwork.data.toy_graphs import karate_club, painters, movie_actor, bow_tie, star_wars
-from sknetwork.visualization.graphs import svg_graph, svg_digraph, svg_bigraph
+from sknetwork.visualization.graphs import svg_graph, svg_bigraph
 
 
 # noinspection DuplicatedCode
@@ -49,31 +49,26 @@ class TestVisualization(unittest.TestCase):
         self.assertEqual(image[1:4], 'svg')
         image = svg_graph(adjacency, position, labels, label_colors=["red", "blue"])
         self.assertEqual(image[1:4], 'svg')
+        image = svg_graph(adjacency, position, labels, node_weights=np.arange(adjacency.shape[0]))
+        self.assertEqual(image[1:4], 'svg')
 
     def test_directed(self):
         graph = painters(True)
         adjacency = graph.adjacency
         position = graph.position
         names = graph.names
-        image = svg_digraph(adjacency, position, names=names)
+        image = svg_graph(adjacency, position, names=names)
         self.assertEqual(image[1:4], 'svg')
         image = svg_graph(adjacency, position, display_edges=False)
         self.assertEqual(image[1:4], 'svg')
         n = adjacency.shape[0]
-        image = svg_digraph(adjacency, position=None, names=np.arange(n), labels=np.arange(n), scores=np.arange(n),
-                            name_position='below', seeds=[0, 1], width=200, height=200, margin=10, margin_text=5,
-                            scale=3, node_order=np.flip(np.arange(n)),
-                            node_size=5, node_size_min=2, node_size_max=6, display_node_weight=True,
-                            node_weights=np.arange(n),
-                            node_width=2, node_width_max=5, node_color='red', edge_width=2, edge_width_min=2,
-                            edge_width_max=4, edge_color='blue', display_edge_weight=True, font_size=14)
-        self.assertEqual(image[1:4], 'svg')
-        image = svg_digraph(adjacency, position=None, names=np.arange(n), labels={0: 1, 1: 2}, scores=np.arange(n),
-                            seeds=[0, 1], width=200, height=200, margin=10, margin_text=5, scale=3,
-                            node_order=np.flip(np.arange(n)), node_size=5, node_size_min=2, node_size_max=6,
-                            display_node_weight=True, node_weights=np.arange(n), node_width=2, node_width_max=5,
-                            node_color='red', edge_width=2, edge_width_min=2, edge_width_max=4, edge_color='blue',
-                            display_edge_weight=True, font_size=14)
+        image = svg_graph(adjacency, position=None, names=np.arange(n), labels=np.arange(n), scores=np.arange(n),
+                          name_position='below', seeds=[0, 1], width=200, height=200, margin=10, margin_text=5,
+                          scale=3, node_order=np.flip(np.arange(n)),
+                          node_size=5, node_size_min=2, node_size_max=6, display_node_weight=True,
+                          node_weights=np.arange(n),
+                          node_width=2, node_width_max=5, node_color='red', edge_width=2, edge_width_min=2,
+                          edge_width_max=4, edge_color='blue', display_edge_weight=True, font_size=14)
         self.assertEqual(image[1:4], 'svg')
 
     def test_bipartite(self):
