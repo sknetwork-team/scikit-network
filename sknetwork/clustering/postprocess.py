ac94fb00ea2923cc1e2c695897c2056559ac4e9b
Thomas Bonald <thomas.bonald@telecom-paristech.fr>
2022-08-31 07:40:15 +0200

    Add function to aggregate graph

--- sknetwork/clustering/postprocess.py
+++ sknetwork/clustering/postprocess.py
@@ -5,7 +5,12 @@ Created on July 10, 2019
 @author: Nathan de Lara <nathan.delara@polytechnique.org>
 @author: Thomas Bonald <bonald@enst.fr>
 """
+from typing import Optional
+
 import numpy as np
+from scipy import sparse
+
+from sknetwork.utils.membership import get_membership
 
 
 def reindex_labels(labels: np.ndarray, consecutive: bool = True) -> np.ndarray:
@@ -37,3 +42,32 @@ def reindex_labels(labels: np.ndarray, consecutive: bool = True) -> np.ndarray:
     _, index = np.unique(sorted_values, return_index=True)
     labels_ = index[labels.astype(int)]
     return labels_
+
+
+def aggregate_graph(input_matrix: sparse.csr_matrix, labels: Optional[np.ndarray] = None,
+                    labels_row: Optional[np.ndarray] = None, labels_col: Optional[np.ndarray] = None) \
+        -> sparse.csr_matrix:
+    """Aggregate graph per label. All nodes with the same label become a single node.
+    Negative labels are ignored (corresponding nodes are not discarded).
+
+    Parameters
+    ----------
+    input_matrix: sparse matrix
+        Adjacency or biadjacency matrix of the graph.
+    labels: np.ndarray
+        Labels of nodes.
+    labels_row: np.ndarray
+        Labels of rows (for bipartite graphs). Alias for labels.
+    labels_col: np.ndarray
+        Labels of columns (for bipartite graphs).
+    """
+    if labels_row is not None:
+        membership_row = get_membership(labels_row)
+    else:
+        membership_row = get_membership(labels)
+    if labels_col is not None:
+        membership_col = get_membership(labels_col)
+    else:
+        membership_col = membership_row
+    aggregate_matrix = membership_row.T.dot(input_matrix).dot(membership_col)
+    return aggregate_matrix
--- sknetwork/clustering/tests/test_post_processing.py
+++ sknetwork/clustering/tests/test_post_processing.py
@@ -6,7 +6,8 @@ import unittest
 
 import numpy as np
 
-from sknetwork.clustering import reindex_labels
+from sknetwork.data import house, star_wars
+from sknetwork.clustering import reindex_labels, aggregate_graph
 
 
 class TestClusteringPostProcessing(unittest.TestCase):
@@ -21,3 +22,19 @@ class TestClusteringPostProcessing(unittest.TestCase):
         labels = np.array([0, 0, 5, 2, 2, 2])
         output = reindex_labels(labels, consecutive=False)
         self.assertTrue(np.array_equal(truth, output))
+
+    def aggregate_graph(self):
+        adjacency = house()
+        labels = np.array([0, 0, 1, 1, 2])
+        aggregate = aggregate_graph(adjacency, labels)
+        self.assertEqual(aggregate.shape, (3, 3))
+
+        biadjacency = star_wars()
+        labels = np.array([0, 0, 1, 2])
+        labels_row = np.array([0, 1, 3, -1])
+        labels_col = np.array([0, 0, 1])
+        aggregate = aggregate_graph(biadjacency, labels, labels_col)
+        self.assertEqual(aggregate.shape, (3, 2))
+        aggregate = aggregate_graph(biadjacency, labels_row, labels_col)
+        self.assertEqual(aggregate.shape, (4, 2))
+
