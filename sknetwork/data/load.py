c91ec20eea651355ed9a20599b2f7f7208374c1d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-31 09:53:29 +0000

    Adding tarfile member sanitization to extractall()

--- sknetwork/data/load.py
+++ sknetwork/data/load.py
@@ -104,7 +104,29 @@ def load_netset(name: Optional[str] = None, data_home: Optional[Union[str, Path]
             raise RuntimeError("Could not reach Netset.")
         with tarfile.open(data_home / (name + '_npz.tar.gz'), 'r:gz') as tar_ref:
             logger.print('Unpacking archive...')
-            tar_ref.extractall(data_home)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_ref, data_home)
         remove(data_home / (name + '_npz.tar.gz'))
 
     files = [file for file in listdir(data_path)]
@@ -179,7 +201,26 @@ def load_konect(name: str, data_home: Optional[Union[str, Path]] = None, auto_nu
                         data_home / (name + '.tar.bz2'))
             with tarfile.open(data_home / (name + '.tar.bz2'), 'r:bz2') as tar_ref:
                 logger.print('Unpacking archive...')
-                tar_ref.extractall(data_home)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar_ref, data_home)
         except (HTTPError, tarfile.ReadError):
             rmdir(data_path)
             raise ValueError('Invalid dataset ' + name + '.'
