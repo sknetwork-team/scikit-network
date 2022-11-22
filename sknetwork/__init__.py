f65bb0e398214ed29461d26903ceb75739ac54a6
Thomas Bonald <thomas.bonald@telecom-paristech.fr>
2022-11-22 17:47:24 +0100

    Bump version: 0.28.0 → 0.28.1

--- sknetwork/__init__.py
+++ sknetwork/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """scikit-network team"""
 __email__ = "thomas.bonald@telecom-paris.fr"
-__version__ = '0.28.0'
+__version__ = '0.28.1'
 
 import sknetwork.topology
 import sknetwork.path
