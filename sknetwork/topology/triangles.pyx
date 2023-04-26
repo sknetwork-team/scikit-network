dd9415e139e6f929102ce97b8bc643d7e6d06a34
Marc Jeanmougin <marc.jeanmougin@telecom-paris.fr>
2023-04-26 16:27:12 +0200

    only use cython linetrace and CYTHON_TRACE_NOGIL on tests

--- sknetwork/topology/triangles.pyx
+++ sknetwork/topology/triangles.pyx
@@ -1,7 +1,5 @@
 # distutils: language = c++
 # cython: language_level=3
-# cython: linetrace=True
-# distutils: define_macros=CYTHON_TRACE_NOGIL=1
 """
 Created on Jun 3, 2020
 @author: Julien Simonnet <julien.simonnet@etu.upmc.fr>
